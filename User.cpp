b280a5d6dcb2bbf8ba45f453285a56de0c59fa70
Hatice Bircan <147690622+HBircan@users.noreply.github.com>
2024-05-15 23:40:11 +0300

    Create User.cpp

---
+++ User.cpp
@@ -0,0 +1,76 @@
+#include <string>
+#include "User.h"
+using namespace std;
+
+User::User (int theUserID, string theUsername, string thePassword, string theEmail, string theContactInfo, string theAccessLevel, string theRole) {
+	UserID = theUserID;
+	Username = theUsername;
+	Password = thePassword;
+	Email = theEmail;
+	ContactInfo = theContactInfo;
+	AccessLevel = theAccessLevel;
+	Role = theRole;
+}
+
+bool User::ValidatePass(string userPassword) const {
+	if (userPassword == Password)
+		return true;
+	else
+		return false;
+}
+//Modifiers
+void  User::setID(int theUserID) {
+	theUserID = UserID;
+}
+
+void  User::setName(string theUsername) {
+	Username = theUsername;
+}
+
+void  User::setPassword(string thePassword) {
+	Password = thePassword;
+}
+
+void  User::setEmail(string theEmail) {
+	Email = theEmail;
+}
+
+void  User::setContact(string theContactInfo) {
+	ContactInfo = theContactInfo;
+}
+
+void  User::setAccess(string theAccessLevel) {
+	AccessLevel = theAccessLevel;
+}
+
+void  User::setRole(string theRole) {
+	Role = theRole;
+}
+//Accessors
+int  User::getID() const {
+	return UserID;
+}
+
+string  User::getName() const{
+	return Username;
+}
+
+string  User::getPassword() const{
+	return Password;
+}
+
+string  User::getEmail() const{
+	return Email;
+}
+
+string  User::getContact() const{
+	return ContactInfo;
+}
+
+string  User::getAccess() const{
+	return AccessLevel;
+}
+
+string  User::getRole() const{
+	return Role;
+}
