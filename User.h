48b16e6b5dee634593a88ae96d5910b9c410d45a
Hatice Bircan <147690622+HBircan@users.noreply.github.com>
2024-05-15 22:17:10 +0300

    Create User.h

---
+++ User.h
@@ -0,0 +1,39 @@
+#ifndef USER_H
+#define USER_H
+#include <string>
+using namespace std;
+
+class User
+{
+private:
+	int UserID;
+	string UserName;
+	string Password;
+	string Email;
+	string ContactInfo;
+	string AccessLevel;
+	string Role;
+
+public:
+	User(int, string, string, string, string, string, string);
+	bool ValidatePass(int);
+
+	void setID(int);
+	void setName(string);
+	void setPassword(string);
+	void setEmail(string);
+	void setContact(string);
+	void setAccess(string);
+	void setRole(string);
+
+	int getID() const;
+	string getName() const;
+	string getPassword() const;
+	string getEmail() const;
+	string getContact() const;
+	string getAccess() const;
+	string getRole() const;
+
+};
+
+#endif
