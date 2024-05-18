fd92f2d393cd2ea43b4b2f6ca62d6bf90f794b83
Hatice Bircan <147690622+HBircan@users.noreply.github.com>
2024-05-18 21:18:38 +0300

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,23 +1,19 @@
-#ifndef LIBRARYBOOK_H
-#define LIBRARYBOOK_H
-
+#ifndef BOOK_H
+#define BOOK_H
+#include "Item.h"
 #include <string>
-
 using namespace std;
 
-class LibraryBook {
+class Book : public Item {
 private:
-    int ISBN;
-    string author;
-
+	long ISBN;
+	string Author;
 public:
-    LibraryBook(int isbn, const std::string& auth);
-    int getISBN() const;
-    string getAuthor() const;
-    void setISBN(int isbn);
-    void setAuthor(const std::string& auth);
-    void display() const;
+	Book(int, string, int, string, string, bool, string, long, string);
+	void setISBN(int);
+	void setAuthor(string);
+	int getISBN() const;
+	string getAuthor() const;
+
 };
-
-#endif 
-
+#endif
