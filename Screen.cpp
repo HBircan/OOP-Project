f01d0a88c662a1eb5002ea2b3f95ba0c567002f9
Hatice Bircan <147690622+HBircan@users.noreply.github.com>
2024-05-18 21:09:59 +0300

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -1,9 +1,26 @@
-#include "LibraryScreen.h"
 #include <iostream>
-
+#include <iomanip>
+#include "Screen.h"
 using namespace std;
 
-void LibraryScreen::Display() const {
-    cout << "Screen is displaying content." << endl;
+ // output a message without a newline
+void Screen::displayMessage(string message) const {
+	 cout << message;
+}
+void Screen::displayMessageLine(string message) const {
+	 cout << message << endl;
 }
 
+void Screen::displayLongNumber(long number) const {
+	cout << number;
+}
+void Screen::displayLongNumberLine(long number) const {
+	cout << number << endl;
+}
+
+void Screen::displayNumber(int number) const {
+	cout << number;
+}
+void Screen::displayNumberLine(int number) const {
+	cout << number << endl;
+}
